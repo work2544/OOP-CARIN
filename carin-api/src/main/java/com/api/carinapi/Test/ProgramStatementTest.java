55f0660f94d989457a966469ad2a7e72ef26841d
thidtanai <thidtanai_k@cmu.ac.th>
2022-03-16 16:33:38 +0700

    Add_Test_from work branch_02

---
+++ carin-api/src/main/java/com/api/carinapi/Test/ProgramStatementTest.java
@@ -0,0 +1,35 @@
+package com.api.carinapi.Test;
+
+import com.api.carinapi.statements.ErrorPack.SyntaxError;
+
+public class ProgramStatementTest {
+    public class MyText{
+        static String[] banned_word={"antibody", "down", "downleft", "downright", "else", "if",
+                "left", "move", "nearby", "right", "shoot", "then", "up", "upleft", "upright", "virus", "while"};
+        static String[] direction={"left" ," right" ," up ", "down "," upleft "," upright "," downleft "," downright"};
+    }
+public static void main(String[] args) throws SyntaxError {
+
+    //Parser Programparser=new Parser("Program/test_genetic");
+}
+//public NodeTree void ParseProgram()
+//    {
+//
+//    }
+//public static NodeTree ParseCommand()
+//{
+//
+//}
+//public static NodeTree ParseBlock(){}
+//    public static NodeTree ParseIF()
+//    {
+//
+//    }
+//    public static NodeTree ParseWhile(){}
+
+
+
+
+}
+
+
